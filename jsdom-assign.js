13d60da37606ae890265fac4c13b319dee0324bd
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-02-16 07:50:58 +0200

    initial release

---
+++ jsdom-assign.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var html =
+    "<!DOCTYPE html>" +
+    "<meta charset=utf-8>" +
+    "<title>shvaikalesh/jsdom-assign - GitHub</title>"
+
+var options =
+{
+    url: "https://github.com/shvaikalesh/jsdom-assign",
+    referrer: "https://github.com/tmpvar/jsdom",
+    cookie:
+        "_octo=GH1.1.1071914994.1455594493; " +
+        "_ga=GA1.2.840814629.1455594493; _gat=1; " +
+        "tz=Europe%2FHelsinki",
+    userAgent:
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) " +
+        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+        "Chrome/48.0.2564.109 Safari/537.36"
+}
+
+var document = require("jsdom").jsdom(html, options)
+
+var window = document.defaultView
+    window.console = global.console
+
+for (var key in window) if (!(key in global))
+{
+    try { global[key] = window[key] }
+    finally {}
+}
